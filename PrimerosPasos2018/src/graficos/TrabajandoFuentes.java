f43c2f28b8dae58af21761c6ad52b81492466f46
juanra1997 <juanrainmortal22@yahoo.com>
2018-09-25 23:48:54 +0200

    +

--- PrimerosPasos2018/src/graficos/TrabajandoFuentes.java
+++ PrimerosPasos2018/src/graficos/TrabajandoFuentes.java
@@ -4,10 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.SystemColor;
+//import java.awt.SystemColor;
 //import java.awt.SystemColor;
 //import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
+//import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
