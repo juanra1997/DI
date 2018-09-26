3b799ca1ddc48aa725087369d84efa1d9098041b
juanra1997 <juanrainmortal22@yahoo.com>
2018-09-26 16:18:47 +0200

    +

--- PrimerosPasos2018/src/practicasPropuestas/CuartaPracticaPropuesta.java
+++ PrimerosPasos2018/src/practicasPropuestas/CuartaPracticaPropuesta.java
@@ -1,9 +1,13 @@
 package practicasPropuestas;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -24,7 +28,7 @@ class NoMovilesVentana extends JFrame {
 	
 	public NoMovilesVentana() {
 		
-		setSize(400,500);
+		setSize(1000,1000);
 		setLocationRelativeTo(null);
 		setTitle("Primer texto en ventana");
 		setResizable(false);
@@ -40,12 +44,35 @@ class NoMoviles extends JPanel {
 	public void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
+		setBackground(Color.WHITE);
 		Graphics2D g2=(Graphics2D)g;
-		Rectangle2D marco=new Rectangle2D.Double(100, 100, 200, 300);
-		g2.setPaint(Color.WHITE);
+		Ellipse2D circulo=new Ellipse2D.Double(100, 100, 800, 800);
+		g2.setPaint(Color.RED);
+		g2.setStroke(new BasicStroke(90f));
+		g2.draw(circulo);
+		/*Ellipse2D circulo1=new Ellipse2D.Double();
+		circulo1.setFrameFromCenter(circulo.getCenterX(), circulo.getCenterY(), 100, 100);
+		g2.draw(circulo1);*/
+		g2.setStroke(new BasicStroke(0f));
+		RoundRectangle2D marco=new RoundRectangle2D.Float(10f, 10f, 10f, 10f, 25f, 25f);
+		marco.setFrameFromCenter(circulo.getCenterX(), circulo.getCenterY(), 350, 250);
+		g2.setPaint(Color.BLACK);
 		g2.fill(marco);
 		g2.draw(marco);
+		Rectangle2D pantalla=new Rectangle2D.Double();
+		pantalla.setFrameFromCenter(circulo.getCenterX(), circulo.getCenterY()-20, 400, 320);
+		g2.setPaint(Color.WHITE);
+		g2.fill(pantalla);
+		g2.draw(pantalla);
 		
+		Ellipse2D circulo2=new Ellipse2D.Double(475, 670, 50, 50);
+		g2.setPaint(Color.WHITE);
+		g2.fill(circulo2);
+		g2.draw(circulo2);
+		
+		g2.setStroke(new BasicStroke(90f));
+		g2.setPaint(Color.RED);
+		g.drawLine(250, 250, 750, 750);
 	}
 	
 }
